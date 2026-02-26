457fe83919b3f0a32d282848829d77cf230932ab
MPhil12 <philippe.maraval@edhec.comcd>
2026-02-26 02:02:49 +0100

    chore(config): ajouter camino4.netlify.app aux origines autorisées CORS

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
     'http://127.0.0.1:3000',
     'https://camino-marseille.netlify.app',
     'https://camino3.netlify.app',
+    'https://camino4.netlify.app',
     process.env.FRONTEND_URL
 ].filter(Boolean);
 
